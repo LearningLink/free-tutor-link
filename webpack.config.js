1da9461d8bafe15530e6a069e201de8d2293bad3
Wayne Wilcox <wilcox_wayne@hotmail.com>
2020-06-25 18:45:59 -0400

    free-tutor link initial folder boilerplate

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+
+module.exports = env => {
+    console.log('NODE_ENV: ', env.NODE_ENV);
+    return {
+        entry: './client/index.js' , 
+        output: {
+            path: path.resolve(__dirname, 'build') ,
+            filename: 'bundle.js'
+        } ,
+          mode: env.NODE_ENV , 
+        devServer: {
+          
+            publicPath: 'http://localhost:8080/build',
+            proxy: {    
+                '/api': { 
+                    target: 'http://localhost:3000',
+                    changeOrigin: true 
+                }
+           
+                }
+            //port: 8080
+          } ,
+        module: {
+            rules: [
+              { test: /\.jsx?/,
+                exclude: /(node_modules)/,
+                use: { 
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env','@babel/preset-react']
+                    }
+                }
+    
+              } , 
+              {
+                test: /\.s[ac]ss$/i,
+                use: [
+                  // Creates `style` nodes from JS strings
+                  'style-loader',
+                  // Translates CSS into CommonJS
+                  'css-loader',
+                  // Compiles Sass to CSS
+                  'sass-loader',
+                ],
+              },
+            ]
+          }
+        }
+    };
