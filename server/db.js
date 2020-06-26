025a9a597d85fc292f0442ddaf3ee0179d116a6a
Wayne Wilcox <wilcox_wayne@hotmail.com>
2020-06-26 14:55:35 -0400

    added database initial file

---
+++ server/db.js
@@ -0,0 +1,14 @@
+const { Pool }  = require('pg');
+
+const PG_URI = "postgres://pbxpcmxr:jfFOHyIzqG5XBjISQWX4UKDPjj3GLGyh@ruby.db.elephantsql.com:5432/pbxpcmxr"
+
+const pool = new Pool({
+	connectionString: PG_URI
+});
+
+module.exports = {
+	query: (text, params, callback) => {
+		console.log('executed query', text);
+		return pool.query(text, params, callback);
+	}
+};
