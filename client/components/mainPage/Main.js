c23689ddad427ce1b8ae4a1d7e8fe9adcf69978b
rosedamasco <rosedamasco@gmail.com>
2020-06-27 12:29:32 -0700

    Connecting frontend to backend with fetch requests

--- client/components/mainPage/Main.js
+++ client/components/mainPage/Main.js
@@ -1,90 +1,57 @@
-import React, { useState, useEffect } from "react";
-import AvailableTutor from "./AvailableTutor";
-import Search from "./Search";
-import Profile from "../profilePage/Profile";
+import React, { useState, useEffect } from 'react';
+import AvailableTutor from './AvailableTutor';
+import Search from './Search';
+import Profile from '../profilePage/Profile';
 
 const Main = (props) => {
-  // const [tutors, setTutors] = useState([]);
   const [error, setError] = useState(null);
-  const [profilePage, setProfilePage] = useState(false);
+  const [choice, setChoice] = useState('');
+  const [availableTutors, setTutors] = useState([]);
 
-  // HOPE TO GET DATA IN ORDER OF DATE/TIME
-  // placeholder data from backend
-  // expected data:
-  const test = {
-    imgUrl: "https://hmp.me/c5v5",
-    name: "Roseanne",
-    linkedInUrl: "http://google.com",
-    date: "2016-05-06",
-    startTime: "12PM",
-    endTime: "12AM",
-    email: "bogus@gmail.com",
-  };
-  const availableTutors = [test];
   const tutors = [];
 
   if (!availableTutors.length) tutors.push(<div>No Tutors Available</div>);
   availableTutors.forEach((tutor) => {
     tutors.push(
       <AvailableTutor
-        imgUrl={tutor.imgUrl}
+        imgUrl={tutor.photo}
         name={tutor.name}
         linkedInUrl={tutor.linkedInUrl}
         date={tutor.date}
-        startTime={tutor.startTime}
-        endTime={tutor.endTime}
+        startTime={tutor.start}
+        endTime={tutor.end}
         email={tutor.email}
       />
     );
   });
 
-  // fetch data from the database
-  // get props for the AvailableTutor component
+  const handleSearch = (e) => {
+    // const skillName = e.target.value;
+    const skillName = choice;
+    fetch(`/availability/${skillName}`)
+      .then((resp) => resp.json())
+      .then(
+        (result) => {
+          console.log('Tutors ', result.tutors);
+          setTutors(result.tutors);
+        },
+        (error) => {
+          setError(error);
+        }
+      );
+  };
 
-  // useEffect(() => {
-  //   fetch('/availability/')
-  //     .then(resp => resp.json())
-  //     .then((result) => {
-  //       console.log(result);
-  //       setTutors(result);
-  //     },
-  //     (error) => {
-  //       setError(error);
-  //     })
-  // }, [])
+  const handleChoice = (e) => {
+    setChoice(e.target.value);
+  };
 
-  function redirect() {
-    setProfilePage(true);
-  }
-  // const handleTimesClick = () => {
-  //   return (
-  //     <div>
-  //       <Profile />
-  //     </div>
-  //   );
-  // };
-  if (profilePage) {
-    return (
-      <div>
-        <Profile />
-      </div>
-    );
-  }
-  // {tutor.email}
-  // {tutor.linkedinprofile}
-  // {tutor.photo}
-  // {tutor.name}
-  // {tutor.start}
-  // {tutor.end}
-
-  // {tutors}
   return (
     <div>
       <div>
         <button onClick={props.handleToProfile}>My Tutoring Times</button>
       </div>
-      <Search />
-      {tutors}
+      <Search handleSearch={handleSearch} handleChoice={handleChoice} choice={choice} />
+      <div>{tutors}</div>
     </div>
   );
 };
--- client/components/profilePage/SkillSet.js
+++ client/components/profilePage/SkillSet.js
@@ -1,16 +1,18 @@
-import React from "react";
+import React, { useEffect } from 'react';
 
 const skillSetSelection = (props) => {
+  useEffect(() => {}, []);
   const checkbox = [];
-  let skills = ["Javascript", "SQL", "HTML", "Python", "Java"];
-  for (let i = 0; i < skills.length; i += 1) {
-    checkbox.push(
-      <div>
-        <input type="checkbox" id="skill " name={`skills${i}`} value="skill" />
-        <label htmlFor={`skills${i}`}>{skills[i]}</label>
-      </div>
-    );
-  }
+  let skills = ['JavaScript', 'SQL', 'React', 'Node Express'];
+  // for (let i = 0; i < skills.length; i += 1) {
+  //   checkbox.push(
+  //     <div>
+  //       <input type="checkbox" id="skill " name={`skills${i}`} value="skill" />
+  //       <label htmlFor={`skills${i}`}>{skills[i]}</label>
+  //     </div>
+  //   );
+  // }
+  checkbox.push(props.skills);
   return (
     <div>
       <h3>Skill Set</h3>
