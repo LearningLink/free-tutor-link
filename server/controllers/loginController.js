73bb86afeceda3f89dceb93905f2c0cd49bb3348
rosedamasco <rosedamasco@gmail.com>
2020-06-26 23:23:50 -0700

    after oauth, sending more request to get user data

--- server/controllers/loginController.js
+++ server/controllers/loginController.js
@@ -1,4 +1,5 @@
-const superagent = require('superagent');
+const request = require('superagent');
+const db = require('../db.js');
 
 const loginController = {};
 
@@ -13,19 +14,47 @@ loginController.sendToLinkedIn = (req, res) =>
 
 loginController.getAccessToken = (req, res, next) => {
   const { code } = req.query;
-  console.log('we in here');
-  superagent
+
+  request
     .post(
       `https://www.linkedin.com/oauth/v2/accessToken?grant_type=authorization_code&code=${code}&redirect_uri=${redirect_uri}&client_id=${client_id}&client_secret=${client_secret}`
     )
-    .end((err, response) => {
-      if (err) return next({ log: `Error in loginController.getAccessToken: ${err}` });
-      res.locals.accessToken = response.body.access_token;
-      console.log('res.locals.accessToken: ', res.locals.accessToken);
-      console.log('cookie bout to set');
+    .then((accessRes) => {
+      res.locals.accessToken = accessRes.body.access_token;
       res.cookie('accessToken', res.locals.accessToken, { httpOnly: true, secure: true });
-      console.log('cookie set');
+      console.log('lev1', res.locals.accessToken);
+      return request
+        .get(
+          'https://api.linkedin.com/v2/me?projection=(id,firstName,lastName,profilePicture(displayImage~:playableStreams))'
+        )
+        .set('Authorization', `Bearer ${res.locals.accessToken}`);
+    })
+    .then((userRes) => {
+      const firstName = userRes.body.firstName.localized.en_US;
+      const lastName = userRes.body.lastName.localized.en_US;
+      const { id } = userRes.body;
+      const imgUrl =
+        userRes.body.profilePicture['displayImage~'].elements[0].identifiers[0].identifier;
+      res.locals.newUser = {
+        name: `${firstName} ${lastName}`,
+        userid: id,
+        imgUrl,
+      };
+      return request
+        .get('https://api.linkedin.com/v2/emailAddress?q=members&projection=(elements*(handle~))')
+        .set('Authorization', `Bearer ${res.locals.accessToken}`);
+    })
+    .then((emailRes) => {
+      const email = emailRes.body.elements[0]['handle~'].emailAddress;
+      res.locals.newUser.email = email;
+      console.log('newUser info: ', res.locals.newUser);
+      // add user to database
       return res.redirect('http://localhost:3000/');
+    })
+    .catch((err) => {
+      return next({
+        log: `Error in loginController.getAccessToken: ${err}`,
+      });
     });
 };
 
